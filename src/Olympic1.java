42260109dcbcf9298049c3c3da8fe276e14cddb5
srashti333 <91120117+srashti333@users.noreply.github.com>
2022-02-08 20:02:25 +0530

    Initial commit

---
+++ src/Olympic1.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+/*The Olympic results are given in a table, where each row represents a country,
+ and columns represent gold, silver, and bronze medals. For example, the element
+ r[0][0] is the number of gold medals earned by the 0-th country. Write a method
+ that takes the table as an input, and returns a new array which lists the total
+ number of medals for each country. For example, if we start with the table
+        2   0   1
+        3   2   0
+        0   0   2
+        in which the 0-th country earned 2 + 0 + 1 = 3 medals, the 1-st country
+         earned 3 + 2 + 0 = 5 medals, and the 2nd country earned 0 + 0 + 2 = 2 medals,
+          your method should return an array with values
+        3
+        5
+        2*/
+class olym
+{
+    int arr[][];
+    int row_size ,col_size;
+    int arrnew[];
+    olym()
+    {
+        Scanner s=new Scanner(System.in);
+        row_size=s.nextInt();
+        col_size=s.nextInt();
+        arr=new int[row_size][col_size];
+        for(int i=0;i<row_size;i++)
+        {
+            for(int j=0;j<col_size;j++)
+            {
+                arr[i][j]=s.nextInt();
+            }
+        }
+        arrnew=new int[row_size];
+    }
+
+    public int[] sum()
+    {
+        for(int i=0;i<row_size;i++)
+        {
+            int c=0;
+            for(int j=0;j<col_size;j++)
+            {
+                c=c+arr[i][j];
+            }
+            arrnew[i]=c;
+        }
+        return arrnew;
+    }
+
+}
+public class Olympic1 {
+    public static void main(String[] args) {
+        Test obj=new Test();
+        int[] arr=obj.sum();
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
